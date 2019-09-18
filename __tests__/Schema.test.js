7aa2ccbbae4476b94b74fe264b32872812e38f11
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-18 10:44:56 -0700

    working through lab 02

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -2,10 +2,20 @@ const Schema = require('../lib/Schema');
 
 describe('Schema', () => {
 
-  // add a test schema...
+  const personSchema = {
+    firstName: { type: 'string', required: true }, 
+    lastName: { type: 'string', required: true },
+    married: { type: 'boolean' },
+    kids: { type: 'number' }
+}
+  
+const schema = new Schema(personSchema) {
+
+}
 
   it('validates a correct model', () => {
 
+    schema.validate(validModel)
   });
 
   it('throws on invalid model', () => {
